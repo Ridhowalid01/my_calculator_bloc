f2bcc7c5bc31152edbc933f26cf91193b51f4567
Ridhowalid01 <rialdifreedom01@gmail.com>
2024-12-07 08:56:14 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_calculator_bloc/screens/home_screen.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
+  }
+}
